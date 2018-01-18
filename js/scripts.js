4c94d7a731af9b4d2b4586da456c4b812eed5753
Chase Bowden and Kayla Ondracek <kayla@>
2018-01-18 08:32:30 -0800

    identify a year as a leap year when divisible by 4

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,9 @@ $(document).ready(function() {
 
     if (userInput % 4 !== 0) {
       alert("get out of here with that non-leap-year garbage");
+    } else if (userInput % 4 === 0) {
+      alert("this could possibly be a leap year")
     }
-  })
+  });
 
 });
